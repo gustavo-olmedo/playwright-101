af11dfcd1223aeda591b3355af8b8e479fdf771e
Gustavo Adolfo Olmedo <gustavo.olmedo.formosa@gmail.com>
2025-12-01 20:02:47 +0000

    update workflow to run the app

--- playwright-tests/playwright.config.ts
+++ playwright-tests/playwright.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
   reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    baseURL: "https://playground-drab-six.vercel.app",
+    baseURL: "http://localhost:3000",
     trace: "on-first-retry",
   },
 
